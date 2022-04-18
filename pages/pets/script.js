f3f9002edfc21f90232dbf671c979334050aaead
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-04-18 16:21:00 +0300

    feat: change color menu

--- pages/pets/script.js
+++ pages/pets/script.js
@@ -1,17 +1,26 @@
 import pets from '../../script/our-pets.js';
 
 const burger = document.querySelector('.burger');
-const body = document.body;
+const page = document.querySelector('.page');
+page.style.height = document.documentElement.clientHeight + 'px';
 
 burger.addEventListener('click', function () {
     burger.classList.toggle('burger_open');
     document.querySelector('.nav-menu').classList.toggle('nav-menu_open');
-    body.classList.toggle('body-hide');
+    page.classList.toggle('page-shadow');
     let navigation = document.querySelector('.nav-menu');
     navigation.addEventListener('click', function (event) {
         if (event.target.classList.contains('paragraph-mobile-menu')) {
             burger.classList.remove('burger_open');
             document.querySelector('.nav-menu').classList.remove('nav-menu_open');
+            page.classList.remove('page-shadow');
+        }
+    });
+    page.addEventListener('click', function (event) {
+        if (event.target.classList.contains('page-shadow')) {
+            burger.classList.remove('burger_open');
+            document.querySelector('.nav-menu').classList.remove('nav-menu_open');
+            page.classList.remove('page-shadow');
         }
     });
 });
