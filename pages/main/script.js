8ff4a98aace8d17dd34a3c4595862e88596d5e33
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-04-18 16:54:18 +0300

    feat: change color menu

--- pages/main/script.js
+++ pages/main/script.js
@@ -24,93 +24,93 @@ burger.addEventListener('click', function () {
         }
     });
 });
-
-function createPetCard(obj) {
-    let petsCard = document.createElement('div'),
-        modalWindow = document.createElement('div'),
-        petImg = document.createElement('img'),
-        name = document.createElement('h4'),
-        name2 = document.createElement('h3'),
-        button = document.createElement('button'),
-        description = document.createElement('p'),
-        breed = document.createElement('p'),
-        list = document.createElement('ul'),
-        age = document.createElement('li'),
-        inoculations = document.createElement('li'),
-        diseases = document.createElement('li'),
-        parasites = document.createElement('li');
-
-    petImg.className = 'pets-card__photo';
-    petImg.src = obj.img;
-    petImg.alt = obj.name;
-    name.className = 'pets-card__name';
-    name.textContent = obj.name;
-    button.className = 'button';
-    button.textContent = 'Learn More';
-    button.classList.add('button_transparent');
-    petsCard.className = 'pets-card';
-    petsCard.append(petImg, name, button);
-
-    description.className = 'pets-card__description';
-    description.textContent = obj.description;
-    breed.className = 'pets-card__breed';
-    breed.textContent = `${obj.type} - ${obj.breed}`;
-    age.innerHTML = `<b>Age:</b> ${obj.age}`;
-    let [...inoculationsList] = obj.inoculations;
-    inoculations.innerHTML = `<b>Inoculations:</b> ${inoculationsList}`;
-    let [...diseasesList] = obj.diseases;
-    diseases.innerHTML = `<b>Diseases:</b> ${diseasesList}`;
-    let [...parasitesList] = obj.parasites;
-    parasites.innerHTML = `<b>Parasites:</b> ${parasitesList}`;
-
-    list.append(age, inoculations, diseases, parasites);
-
-    name2.className = 'pets-card__name';
-    name2.textContent = obj.name;
-
-    modalWindow.append(name2, breed, description, list);
-    modalWindow.className = 'modal-window';
-    modalWindow.style.display = 'none';
-
-    petsCard.append(modalWindow);
-
-    return petsCard;
-}
-
-
-let slider = document.querySelector('.slider'),
-    petsCards = slider.querySelector('.pets-cards'),
-    arrowLeft = slider.querySelector('.arrow-left'),
-    arrowRight = slider.querySelector('.arrow-right');
-
-// pets.forEach(pet => {
-//     petsCards.append(createPetCard(pet));
-// });
-
-
-let slides = petsCards.querySelectorAll('.pets-card'),
-    slideWidth = slides[0].offsetWidth,
-    slideIndex = 0,
-    transition = true,
-    slide = function () {
-        if (transition) {
-            petsCards.style.transition = 'transform .5s';
-        }
-        petsCards.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
-    };
-
-
-// slider.addEventListener('click', function () {
-//     let target = event.target;
 //
-//     if (target.classList.contains('arrow-left')) {
-//         slideIndex--;
-//     } else if (target.classList.contains('arrow-right')) {
-//         slideIndex++;
-//     } else {
-//         return;
-//     }
+// function createPetCard(obj) {
+//     let petsCard = document.createElement('div'),
+//         modalWindow = document.createElement('div'),
+//         petImg = document.createElement('img'),
+//         name = document.createElement('h4'),
+//         name2 = document.createElement('h3'),
+//         button = document.createElement('button'),
+//         description = document.createElement('p'),
+//         breed = document.createElement('p'),
+//         list = document.createElement('ul'),
+//         age = document.createElement('li'),
+//         inoculations = document.createElement('li'),
+//         diseases = document.createElement('li'),
+//         parasites = document.createElement('li');
 //
-//     slide();
-// });
-
+//     petImg.className = 'pets-card__photo';
+//     petImg.src = obj.img;
+//     petImg.alt = obj.name;
+//     name.className = 'pets-card__name';
+//     name.textContent = obj.name;
+//     button.className = 'button';
+//     button.textContent = 'Learn More';
+//     button.classList.add('button_transparent');
+//     petsCard.className = 'pets-card';
+//     petsCard.append(petImg, name, button);
+//
+//     description.className = 'pets-card__description';
+//     description.textContent = obj.description;
+//     breed.className = 'pets-card__breed';
+//     breed.textContent = `${obj.type} - ${obj.breed}`;
+//     age.innerHTML = `<b>Age:</b> ${obj.age}`;
+//     let [...inoculationsList] = obj.inoculations;
+//     inoculations.innerHTML = `<b>Inoculations:</b> ${inoculationsList}`;
+//     let [...diseasesList] = obj.diseases;
+//     diseases.innerHTML = `<b>Diseases:</b> ${diseasesList}`;
+//     let [...parasitesList] = obj.parasites;
+//     parasites.innerHTML = `<b>Parasites:</b> ${parasitesList}`;
+//
+//     list.append(age, inoculations, diseases, parasites);
+//
+//     name2.className = 'pets-card__name';
+//     name2.textContent = obj.name;
+//
+//     modalWindow.append(name2, breed, description, list);
+//     modalWindow.className = 'modal-window';
+//     modalWindow.style.display = 'none';
+//
+//     petsCard.append(modalWindow);
+//
+//     return petsCard;
+// }
+//
+//
+// let slider = document.querySelector('.slider'),
+//     petsCards = slider.querySelector('.pets-cards'),
+//     arrowLeft = slider.querySelector('.arrow-left'),
+//     arrowRight = slider.querySelector('.arrow-right');
+//
+// // pets.forEach(pet => {
+// //     petsCards.append(createPetCard(pet));
+// // });
+//
+//
+// let slides = petsCards.querySelectorAll('.pets-card'),
+//     slideWidth = slides[0].offsetWidth,
+//     slideIndex = 0,
+//     transition = true,
+//     slide = function () {
+//         if (transition) {
+//             petsCards.style.transition = 'transform .5s';
+//         }
+//         petsCards.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
+//     };
+//
+//
+// // slider.addEventListener('click', function () {
+// //     let target = event.target;
+// //
+// //     if (target.classList.contains('arrow-left')) {
+// //         slideIndex--;
+// //     } else if (target.classList.contains('arrow-right')) {
+// //         slideIndex++;
+// //     } else {
+// //         return;
+// //     }
+// //
+// //     slide();
+// // });
+//
--- pages/pets/script.js
+++ pages/pets/script.js
@@ -24,74 +24,74 @@ burger.addEventListener('click', function () {
         }
     });
 });
-
-function createPetCard(obj) {
-    let petsCard = document.createElement('div'),
-        modalWindow = document.createElement('div'),
-        petImg = document.createElement('img'),
-        name = document.createElement('h4'),
-        name2 = document.createElement('h3'),
-        button = document.createElement('button'),
-        description = document.createElement('p'),
-        breed = document.createElement('p'),
-        list = document.createElement('ul'),
-        age = document.createElement('li'),
-        inoculations = document.createElement('li'),
-        diseases = document.createElement('li'),
-        parasites = document.createElement('li');
-
-    petImg.className = 'pets-card__photo';
-    petImg.src = obj.img;
-    petImg.alt = obj.name;
-    name.className = 'pets-card__name';
-    name.textContent = obj.name;
-    button.className = 'button';
-    button.textContent = 'Learn More';
-    button.classList.add('button_transparent');
-    petsCard.className = 'pets-card';
-    petsCard.append(petImg, name, button);
-
-    description.className = 'pets-card__description';
-    description.textContent = obj.description;
-    breed.className = 'pets-card__breed';
-    breed.textContent = `${obj.type} - ${obj.breed}`;
-    age.innerHTML = `<b>Age:</b> ${obj.age}`;
-    let [...inoculationsList] = obj.inoculations;
-    inoculations.innerHTML = `<b>Inoculations:</b> ${inoculationsList}`;
-    let [...diseasesList] = obj.diseases;
-    diseases.innerHTML = `<b>Diseases:</b> ${diseasesList}`;
-    let [...parasitesList] = obj.parasites;
-    parasites.innerHTML = `<b>Parasites:</b> ${parasitesList}`;
-
-    list.append(age, inoculations, diseases, parasites);
-
-    name2.className = 'pets-card__name';
-    name2.textContent = obj.name;
-
-    modalWindow.append(name2, breed, description, list);
-    modalWindow.className = 'modal-window';
-    modalWindow.style.display = 'none';
-
-    petsCard.append(modalWindow);
-
-    return petsCard;
-}
-
-let petsCards = document.querySelector('.pets-cards');
-
-// pets.forEach(pet => {
-//     petsCards.append(createPetCard(pet));
-// });
-
-
-let arrowLeft = document.querySelector('.arrow-left'),
-    arrowRight = document.querySelector('.arrow-right');
-
-// arrowLeft.addEventListener('click', function (){
-//     petsCards.scrollLeft += 360;
-//     console.log(1321);
-// })
 //
-// arrowRight.addEventListener('click', function (){
-//     petsCards.scrollLeft -= 360;
+// function createPetCard(obj) {
+//     let petsCard = document.createElement('div'),
+//         modalWindow = document.createElement('div'),
+//         petImg = document.createElement('img'),
+//         name = document.createElement('h4'),
+//         name2 = document.createElement('h3'),
+//         button = document.createElement('button'),
+//         description = document.createElement('p'),
+//         breed = document.createElement('p'),
+//         list = document.createElement('ul'),
+//         age = document.createElement('li'),
+//         inoculations = document.createElement('li'),
+//         diseases = document.createElement('li'),
+//         parasites = document.createElement('li');
+//
+//     petImg.className = 'pets-card__photo';
+//     petImg.src = obj.img;
+//     petImg.alt = obj.name;
+//     name.className = 'pets-card__name';
+//     name.textContent = obj.name;
+//     button.className = 'button';
+//     button.textContent = 'Learn More';
+//     button.classList.add('button_transparent');
+//     petsCard.className = 'pets-card';
+//     petsCard.append(petImg, name, button);
+//
+//     description.className = 'pets-card__description';
+//     description.textContent = obj.description;
+//     breed.className = 'pets-card__breed';
+//     breed.textContent = `${obj.type} - ${obj.breed}`;
+//     age.innerHTML = `<b>Age:</b> ${obj.age}`;
+//     let [...inoculationsList] = obj.inoculations;
+//     inoculations.innerHTML = `<b>Inoculations:</b> ${inoculationsList}`;
+//     let [...diseasesList] = obj.diseases;
+//     diseases.innerHTML = `<b>Diseases:</b> ${diseasesList}`;
+//     let [...parasitesList] = obj.parasites;
+//     parasites.innerHTML = `<b>Parasites:</b> ${parasitesList}`;
+//
+//     list.append(age, inoculations, diseases, parasites);
+//
+//     name2.className = 'pets-card__name';
+//     name2.textContent = obj.name;
+//
+//     modalWindow.append(name2, breed, description, list);
+//     modalWindow.className = 'modal-window';
+//     modalWindow.style.display = 'none';
+//
+//     petsCard.append(modalWindow);
+//
+//     return petsCard;
+// }
+//
+// let petsCards = document.querySelector('.pets-cards');
+//
+// // pets.forEach(pet => {
+// //     petsCards.append(createPetCard(pet));
+// // });
+//
+//
+// let arrowLeft = document.querySelector('.arrow-left'),
+//     arrowRight = document.querySelector('.arrow-right');
+//
+// // arrowLeft.addEventListener('click', function (){
+// //     petsCards.scrollLeft += 360;
+// //     console.log(1321);
+// // })
+// //
+// // arrowRight.addEventListener('click', function (){
+// //     petsCards.scrollLeft -= 360;
 // })
