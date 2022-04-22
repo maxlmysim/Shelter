32c3ce30404cf8d5e7c554cc1c2407b9a1c191df
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-04-22 13:39:18 +0300

    feat: add modal-window

--- pages/main/script.js
+++ pages/main/script.js
@@ -5,6 +5,7 @@ const page = document.querySelector('.page');
 page.style.height = document.documentElement.clientHeight + 'px';
 
 burger.addEventListener('click', function () {
+    document.querySelector("body").classList.toggle('no-scroll');
     burger.classList.toggle('burger_open');
     document.querySelector('.nav-menu').classList.toggle('nav-menu_open');
     page.classList.toggle('page-shadow');
@@ -14,10 +15,13 @@ burger.addEventListener('click', function () {
             burger.classList.remove('burger_open');
             document.querySelector('.nav-menu').classList.remove('nav-menu_open');
             page.classList.remove('page-shadow');
+            document.querySelector("body").classList.remove('no-scroll');
+
         }
     });
     page.addEventListener('click', function (event) {
         if (event.target.classList.contains('page-shadow')) {
+            document.querySelector("body").classList.remove('no-scroll');
             burger.classList.remove('burger_open');
             document.querySelector('.nav-menu').classList.remove('nav-menu_open');
             page.classList.remove('page-shadow');
@@ -29,16 +33,18 @@ function createPetCard(obj, id) {
     let petsCard = document.createElement('div'),
         modalWindow = document.createElement('div'),
         petImg = document.createElement('img'),
+        petImg2 = document.createElement('img'),
         name = document.createElement('h4'),
         name2 = document.createElement('h3'),
         button = document.createElement('button'),
-        description = document.createElement('p'),
-        breed = document.createElement('p'),
+        description = document.createElement('h5'),
+        breed = document.createElement('h4'),
         list = document.createElement('ul'),
         age = document.createElement('li'),
         inoculations = document.createElement('li'),
         diseases = document.createElement('li'),
-        parasites = document.createElement('li');
+        parasites = document.createElement('li'),
+        buttonCloseModal = document.createElement('div');
 
     petImg.className = 'pets-card__photo';
     petImg.src = obj.img;
@@ -56,21 +62,36 @@ function createPetCard(obj, id) {
     breed.className = 'pets-card__breed';
     breed.textContent = `${obj.type} - ${obj.breed}`;
     age.innerHTML = `<b>Age:</b> ${obj.age}`;
+    age.className = 'h5-modal-window';
     let [...inoculationsList] = obj.inoculations;
-    inoculations.innerHTML = `<b>Inoculations:</b> ${inoculationsList}`;
+    inoculations.innerHTML = `<b>Inoculations:</b> ${inoculationsList.join(', ')}`;
+    inoculations.className = 'h5-modal-window'
     let [...diseasesList] = obj.diseases;
-    diseases.innerHTML = `<b>Diseases:</b> ${diseasesList}`;
+    diseases.innerHTML = `<b>Diseases:</b> ${diseasesList.join(', ')}`;
+    diseases.className = 'h5-modal-window'
     let [...parasitesList] = obj.parasites;
-    parasites.innerHTML = `<b>Parasites:</b> ${parasitesList}`;
+    parasites.innerHTML = `<b>Parasites:</b> ${parasitesList.join(', ')}`;
+    parasites.className = 'h5-modal-window'
 
     list.append(age, inoculations, diseases, parasites);
+    list.className = 'pets-card__extra-description'
 
     name2.className = 'pets-card__name';
     name2.textContent = obj.name;
 
-    modalWindow.append(name2, breed, description, list);
-    modalWindow.className = 'pets-card__modal-window';
-    modalWindow.style.display = 'none';
+    let wrapper = document.createElement('div');
+    let modalContent = document.createElement('div');
+    modalContent.className = 'modal-window__content'
+    wrapper.className = 'modal-window__wrapper';
+    petImg2.className = 'pets-card__photo';
+    petImg2.src = obj.img;
+    petImg2.alt = obj.name;
+    modalContent.append(name2, breed, description, list)
+    wrapper.append(petImg2, modalContent)
+    buttonCloseModal.className = 'btn-close-window'
+    modalWindow.append(wrapper, buttonCloseModal);
+    modalWindow.className = 'modal-window';
+    modalWindow.classList.add('hidden');
 
     petsCard.append(modalWindow);
     petsCard.dataset.id = id;
@@ -133,7 +154,9 @@ arrowLeft.addEventListener('click', moveLeft);
 arrowRight.addEventListener('click', moveRight);
 
 carousel.addEventListener('animationend', function (animationEvent) {
-    if (animationEvent.animationName === 'move-left') {
+    if (animationEvent.animationName === 'move-left-1card' ||
+        animationEvent.animationName === 'move-left-2card' ||
+        animationEvent.animationName === 'move-left-3card') {
         itemRight.innerHTML = itemActive.innerHTML;
         itemActive.innerHTML = itemLeft.innerHTML;
         itemLeft.innerHTML = '';
@@ -151,4 +174,28 @@ carousel.addEventListener('animationend', function (animationEvent) {
         carousel.classList.remove('transition-right');
     }
 });
+
+carousel.addEventListener('click', function (event) {
+    let card = event.target.closest('.pets-card');
+    let btnClose = card.querySelector('.btn-close-window')
+    if (card) {
+        card.children[3].classList.remove('hidden');
+        page.classList.add('page-shadow');
+        document.querySelector("body").classList.add('no-scroll');
+        let close = (event) => {
+            if (event.target.classList.contains('page-shadow') ||
+                event.target.classList.contains('btn-close-window')) {
+                document.querySelector("body").classList.remove('no-scroll');
+                page.classList.remove('page-shadow');
+                card.children[3].classList.add('hidden');
+                page.removeEventListener('click', close)
+                btnClose.removeEventListener('click', close)
+            }
+        }
+        page.addEventListener('click', close);
+        btnClose.addEventListener('click', close);
+    }
+});
+
+
 
